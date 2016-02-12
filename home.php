ba062981a9a9f68daccaf8b97ecd7cc7a266a400
blazerzero <habibelo@oregonstate.edu>
2016-02-11 21:41:49 -0800

    BIG update with this push:
    * NEW page where viewer can view and edit their book postings
    * redesigning the interface, including a new color scheme and fonts
    * NEW quick navigation buttons at the bottom right of each page
      (this will come in handy when the user has scrolled down beyond
      the view of the navigation bar, as the quick nav buttons will
      always remain at the bottom right of the page)
    * the problem i was having with viewing the welcome page on the
      ENGR server has been fixed. the problem was that whatever file
      is named "index" is automatically redirected to when clicking on
      the BeaverBooks folder on my webpage. so welcome.html is now
      index.html and index.php is now home.php
    * cleaning up the pages and making other minor changes
    
    Let me know what you guys think!
    
    -Omeed

---
+++ home.php
@@ -0,0 +1,73 @@
+<?php include "./header.php" ?>
+
+<?php
+	if (checkAuth(true) != "") {
+?>
+
+<html>
+  <head>
+    <title>BeaverBooks</title>
+    <link type="text/css" rel="stylesheet" href="./css/stylesheet.css"/>
+    <link type="text/css" rel="stylesheet" href="./bower_components/semantic/dist/semantic.css"/>
+  </head>
+  <body>
+    <br><br>
+    <left class="sitename"> BEAVERBOOKS </left>
+
+  <!--    <div>
+        <center><h2><strong>Find a book you need, or sell your old one!</h2></strong></center>
+      </div> -->
+    <div>
+      <ul class="navbar">
+        <li class="active"><a href="./home.php">Home</a></li>
+				<li><a href="./yourbooks.html">Your Books</a></li>
+        <li><a href="./booksell.html">Sell A Book</a></li>
+        <li><a href="./searchpage.html">Search Books</a></li>
+        <li><a href="./locationpage.html">Books Near You</a></li>
+        <li><a href="./about.html">About</a></li>
+        <li style="float:right"><a href="./logout.php">Logout</a></li>
+      </ul>
+
+      <div>
+        <center><h1>Recent Posts</h1></center>
+      </div>
+
+			<div class="ui divider"></div>
+
+      <div class="textbody">
+        <center><p> This is where we put stuff, like recents </p></center>
+      </div>
+			<br>
+
+			<div align="right" position="absolute">
+				<a href="./booksell.html">
+					<button class="circular ui icon button one">
+						<i class="plus icon"></i>
+						<p>Sell A Book</p>
+					</button>
+				</a>
+				<a href="yourbooks.html">
+					<button class="circular ui icon button two">
+						<i class="book icon"></i>
+						<p>Your Books</p>
+					</button>
+				</a>
+				<a href="searchpage.html">
+					<button class="circular ui icon button three">
+						<i class="search icon"></i>
+						<p>Search</p>
+					</button>
+				</a>
+				<a href="locationpage.html">
+					<button class="circular ui icon button four">
+						<i class="location arrow icon"></i>
+						<p>Near You</p>
+					</button>
+				</a>
+			</div>
+
+    </div>
+  </body>
+</html>
+
+<?php } ?>
