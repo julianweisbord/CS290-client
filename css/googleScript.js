1737c4cf2db069f1532cc178d644205b59c1e084
Brennan Giles <collegegiles@gmail.com>
2016-02-13 18:00:43 -0800

    Org changes
    files that don’t need to be php fixed to html again, and i included my
    progress on the location page thus far.

---
+++ css/googleScript.js
@@ -0,0 +1,67 @@
+function map(version) {
+
+// if version 1, then it is current location, if 2, it is at zip code
+
+
+if (version == 1){
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+    var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="
+    +latlon+"&zoom=14&size=400x300&sensor=false";
+    document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+          }
+    }
+  }
+}
+
+
+
+
+
+if (version == 2){
+
+var zip;
+zip = document.getElementById("zip").value;
+
+var lat = '';
+var lng = '';
+
+    geocoder.geocode( { 'address': zip}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+         lat = results[0].geometry.location.lat();
+         lng = results[0].geometry.location.lng();
+        });
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+    alert('Latitude: ' + lat + ' Logitude: ' + lng);
+
+
+
+}
